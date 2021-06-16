40f2f87a8cd57020b8c9a768f073a8c25ffab37e
Vishnu R <vishnu.ravi.543@gmail.com>
2021-06-16 18:49:13 +0530

    updated issues

--- src/test/java/in/vishnu/util/EmailValdationTest.java
+++ src/test/java/in/vishnu/util/EmailValdationTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import in.vishnu.validation.EmailValidation;
+
 class EmailValdationTest {
 
 	/**
@@ -17,7 +19,7 @@ class EmailValdationTest {
 		"arunachalam@gmail.com, admin123@gmail.com, aashikahamed@gmail.com"
 	})
 	void validPasswordCase(String email) {
-		boolean isValid = Util.isEmailValid(email);
+		boolean isValid = EmailValidation.isEmailValid(email);
 		assertTrue(isValid);
 	}
 	
@@ -27,7 +29,7 @@ class EmailValdationTest {
 		"Admin01gmail.com, @Admin01gmailcom, Admin01gmailcom@, @Admin01gmail@com@"
 	})
 	void invalidPasswordCase(String email) {
-		boolean isValid = Util.isEmailValid(email);
+		boolean isValid = EmailValidation.isEmailValid(email);
 		assertFalse(isValid);
 	}
 
